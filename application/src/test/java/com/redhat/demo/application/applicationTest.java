b17a2cbe3e121e1a5356b4c6f74a4528bd88ad2f
Chiang.www <Chiang.www@d9647ef5-e1ce-1ef7-ec47-7e35b179a5d7>
2010-05-15 14:16:14 +0000

    单元测试

---
+++ application/src/test/java/com/redhat/demo/application/applicationTest.java
@@ -0,0 +1,35 @@
+package com.redhat.demo.application;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.dayatang.springtest.PureSpringTestCase;
+import com.redhat.demo.domain.FunctionEntity;
+
+public class applicationTest extends PureSpringTestCase {
+	
+	private FunctionEntityApplication app;
+
+@Override
+public void setup() {
+	super.setup();
+	app = context.getBean(FunctionEntityApplication.class);
+}
+
+@Override
+protected String[] springXmlPath() {
+	return new String[] { "classpath*:spring/*.xml" };
+}
+
+@Test
+public void testGetFunctionEntityById(){
+	FunctionEntity entity = null;
+	try {
+		entity = app.getFunctionEntityById(999999L);
+		Assert.assertNotNull(entity);
+	} catch (RuntimeException e) {
+		Assert.assertNull(entity);
+	}
+	
+}
+}
